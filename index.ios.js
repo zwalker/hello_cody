5ff45af001a6ff2409e8e565989cfbec5dc47590
Zach Walker <zach.walker@appfolio.com>
2017-01-04 17:16:51 -0800

    zw - initial commit of hello work project
    Generated using react-native init hello_cody

---
+++ index.ios.js
@@ -0,0 +1,59 @@
+/**
+ * Sample React Native App
+ * https://github.com/facebook/react-native
+ * @flow
+ */
+
+import React, { Component } from 'react';
+import {
+  AppRegistry,
+  StyleSheet,
+  Text,
+  View
+} from 'react-native';
+
+export default class hello_cody extends Component {
+
+  handleClick() {
+    console.log("Clickity Click");
+  }
+
+  render() {
+    console.log("poop");
+    return (
+      <View style={styles.container}>
+        <Text style={styles.welcome} onPress={this.handleClick.bind(this)}>
+          Welcome to React Native!
+        </Text>
+        <Text style={styles.instructions}>
+          To get started, edit index.ios.js
+        </Text>
+        <Text style={styles.instructions}>
+          Press Cmd+R to reload,{'\n'}
+          Cmd+D or shake for dev menu
+        </Text>
+      </View>
+    );
+  }
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center',
+    backgroundColor: '#F5FCFF',
+  },
+  welcome: {
+    fontSize: 55,
+    textAlign: 'center',
+    margin: 10,
+  },
+  instructions: {
+    textAlign: 'center',
+    color: '#333333',
+    marginBottom: 5,
+  },
+});
+
+AppRegistry.registerComponent('hello_cody', () => hello_cody);
