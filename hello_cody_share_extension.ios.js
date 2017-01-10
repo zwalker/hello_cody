0ab7576cebdf0616c0f45c8b776a90b94c8459ce
Zach Walker <zach.walker@appfolio.com>
2017-01-09 23:23:39 -0800

    zw - WIP, adding a picker to select a tag for the post
    Checkout https://github.com/l-urence/react-native-autocomplete-input
    before moving on

--- hello_cody_share_extension.ios.js
+++ hello_cody_share_extension.ios.js
@@ -7,6 +7,7 @@ import {
   Text,
   TextInput,
   View,
+  Picker,
   TouchableOpacity
 } from 'react-native'
 
@@ -16,48 +17,88 @@ class HelloCodyShareExtension extends Component {
     this.state = {
       isOpen: true,
       type: '',
-      value: ''
+      value: '',
+      tags: {},
+      selectedTag: null
     }
+  }
+
+  fetchTags() {
+    return new Promise((resolve, reject) => {
+      resolve({
+        body: JSON.stringify({
+          'Big Bird': 1,
+          'Hobbit': 2,
+          'Batman': 3
+        })
+      })
+    });
   }
 
   async componentDidMount() {
     try {
-      const { type, value } = await ShareExtension.data()
-      this.setState({
-        type,
-        value
+      const { type, value } = await ShareExtension.data();
+      this.fetchTags()
+      .then(response => {
+        tags = JSON.parse(response.body);
+        this.setState({
+          type,
+          value,
+          tags
+        })
       })
+      .catch(error => {
+        let tags = error;
+        this.setState({
+          type,
+          value,
+          tags
+        })
+      });
     } catch(e) {
       console.log('errrr', e)
     }
   }
 
-  onClose() {
-    ShareExtension.close()
+  selectTag(tagId) {
+    console.log('selected tag', tagId);
+    this.setState({
+      selectedTag: tagId
+    });
   }
 
-  closing = () => {
+  onClose() {
+    ShareExtension.close();
+  }
+
+  closing() {
     this.setState({
       isOpen: false
-    })
+    });
   }
 
   render() {
+    console.log(Object.entries(this.state.tags), 'butthole');
+    let pickerItems = Object.entries(this.state.tags).map(([tag, tagId]) => (<Picker.Item label={ tag } value={ tagId } key={ tagId }/>));
     return (
       <Modal backdrop={false}
       style={{ backgroundColor: 'transparent' }} position="center" isOpen={this.state.isOpen} onClosed={this.onClose}>
         <View style={{ alignItems: 'center', justifyContent:'center', flex: 1 }}>
           <View style={{ borderColor: 'grey', borderRadius: 10, borderWidth: 1, backgroundColor: 'white', height: 200, width: 300, overflow: 'hidden' }}>
             <View style={{ backgroundColor: 'grey', height: 50, padding: 10, flexDirection: 'row', justifyContent: 'flex-start', alignItems: 'center' }}>
-              <Text style={{ width: 50, textAlign: 'left' }}onPress={ this.closing }>Cancel</Text>
+              <Text style={{ width: 50, textAlign: 'left' }}onPress={ this.closing.bind(this) }>Cancel</Text>
               <Text style={{ flex: 1, textAlign: 'center', fontWeight: 'bold' }}>Hello Cody</Text>
-              <Text style={{ width: 50, textAlign: 'right' }} onPress={ this.closing }>Post</Text>
+              <Text style={{ width: 50, textAlign: 'right' }} onPress={ this.closing.bind(this) }>Post</Text>
             </View>
             <View>
-              <TouchableOpacity onPress={this.closing}>
+              <TouchableOpacity onPress={this.closing.bind(this)}>
                 <Text>Close</Text>
                 <Text>type: { this.state.type }</Text>
                 <Text>value: { this.state.value }</Text>
+                <Picker selectedValue={ this.state.tags } onValueChange={ this.selectTag.bind(this) }>
+                  { pickerItems }
+                </Picker>
+                <Text>poop</Text>
               </TouchableOpacity>
             </View>
           </View>
