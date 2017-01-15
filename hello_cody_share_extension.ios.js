18523de822d584971de0847366cabdcc8b210e65
Zach Walker <zach.walker@appfolio.com>
2017-01-15 06:54:58 -0800

    zw - add ability to select a tag
    Uses the navigator to render the select tag view when the tag bar at the bottom of the share view is pressed

--- hello_cody_share_extension.ios.js
+++ hello_cody_share_extension.ios.js
@@ -9,13 +9,15 @@ import {
   View,
   Picker,
   TouchableOpacity,
-  Navigator
+  Navigator,
+  ListView
 } from 'react-native'
 
 class HelloCodyShareExtension extends Component {
   constructor(props, context) {
     super(props, context)
 
+    this.dataSource = new ListView.DataSource({rowHasChanged: (r1,r2) => r1 !== r2});
     this.nav = null;
     this.state = {
       isOpen: true,
@@ -92,6 +94,8 @@ class HelloCodyShareExtension extends Component {
     switch (route.id) {
       case "Share":
         return this.renderShareView(nav);
+      case "SelectTag":
+        return this.renderSelectTag(nav);
       default:
         return this.renderShareView(nav);
     }
@@ -106,7 +110,7 @@ class HelloCodyShareExtension extends Component {
         <View style={{ alignItems: 'center', justifyContent:'center', flex: 1 }}>
           <View style={{ borderColor: 'grey', borderRadius: 10, borderWidth: 1, backgroundColor: 'white', height: 200, width: 300, overflow: 'hidden' }}>
             <View style={{ backgroundColor: 'grey', height: 50, padding: 10, flexDirection: 'row', justifyContent: 'flex-start', alignItems: 'center' }}>
-              <Text style={{ width: 50, textAlign: 'left' }}onPress={ this.closing.bind(this) }>Cancel</Text>
+              <Text style={{ width: 50, textAlign: 'left' }} onPress={ this.closing.bind(this) }>Cancel</Text>
               <Text style={{ flex: 1, textAlign: 'center', fontWeight: 'bold' }}>Hello Cody</Text>
               <Text style={{ width: 50, textAlign: 'right' }} onPress={ this.closing.bind(this) }>Post</Text>
             </View>
@@ -118,10 +122,60 @@ class HelloCodyShareExtension extends Component {
                 <Text>poop</Text>
               </TouchableOpacity>
             </View>
-            <View style={{ backgroundColor: 'grey', flexDirection: 'row', alignItems: 'center', padding: 10, height: 50 }}>
+            <View style={{ backgroundColor: 'grey', padding: 10, height: 50 }} >
+              <TouchableOpacity style={{ flexDirection: 'row', alignItems: 'center' }} onPress={ () => nav.push({ id: "SelectTag" }) } >
                 <Text style={{ fontWeight: 'bold', textAlign: 'left', flex: 1 }}>Tag</Text>
-                <Text style={{ textAlign: 'right' }}>Blah ></Text>
+                <Text style={{ textAlign: 'right' }}>{ this.state.selectedTag || Object.keys(this.state.tags)[0] } ></Text>
+              </TouchableOpacity>
             </View>
+          </View>
+        </View>
+      </Modal>
+    )
+  }
+
+  handleTagSelected(selectedTag) {
+    console.log("handleTagSelected", selectedTag);
+    this.setState({selectedTag});
+    this.nav.pop();
+  }
+
+  renderRow(rowData, sectionId, rowId) {
+    console.log("rendering", rowData);
+    return (
+      <TouchableOpacity onPress={ () => this.handleTagSelected(rowData) }>
+        <View style={{ height: 25, backgroundColor: 'blue', flexDirection: 'row', alignItems: 'center', paddingLeft: 5 }}>
+          <Text style={{ textAlign: 'center' }}>{rowData}</Text>
+        </View>
+      </TouchableOpacity>
+    );
+  }
+
+  renderSeparator(sectionId, rowId, adjacentRowHighlighted) {
+    let key = `${sectionId}-${rowId}`;
+    let height = adjacentRowHighlighted ? 4 : 1;
+    let backgroundColor = adjacentRowHighlighted ? '#3B5998' : '#CCCCCC';
+    return (
+      <View key={key} style={{ height, backgroundColor }} />
+    ); 
+  }
+
+  renderSelectTag(nav) {
+    console.log("!!!!!!!!!!!", Object.keys(this.state.tags));
+    return (
+      <Modal backdrop={false}
+      style={{ backgroundColor: 'transparent' }} position="center" isOpen={this.state.isOpen} onClosed={this.onClose}>
+        <View style={{ alignItems: 'center', justifyContent:'center', flex: 1 }}>
+          <View style={{ borderColor: 'grey', borderRadius: 10, borderWidth: 1, backgroundColor: 'white', height: 200, width: 300, overflow: 'hidden' }}>
+            <View style={{ backgroundColor: 'grey', height: 50, padding: 10, flexDirection: 'row', justifyContent: 'flex-start', alignItems: 'center' }}>
+              <Text style={{ width: 50, textAlign: 'left' }} onPress={ () => nav.pop() }>Back</Text>
+            </View>
+            <ListView style={{  }}
+                      removeClippedSubviews = {false}
+                      dataSource={ this.dataSource.cloneWithRows(Object.keys(this.state.tags)) } 
+                      renderRow={ this.renderRow.bind(this) }
+                      renderSeparator={ this.renderSeparator.bind(this) }
+             />
           </View>
         </View>
       </Modal>
