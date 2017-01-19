ff76e6fa434d52d4fa00b837e7c2bcda9002d8df
Zach Walker <zach.walker@appfolio.com>
2017-01-19 14:02:41 -0800

    zw - adding the ShareActivity as a processor of the SEND intent

---
+++ android/app/src/main/java/com/hello_cody/share/ShareActivity.java
@@ -0,0 +1,11 @@
+package com.hello_cody.share;
+ 
+import com.facebook.react.ReactActivity;
+
+public class ShareActivity extends ReactActivity {
+  @Override
+  protected String getMainComponentName() {
+    // this is the name AppRegistry will use to launch the Share View 
+    return "HelloCodyShareExtension";
+  }
+}
